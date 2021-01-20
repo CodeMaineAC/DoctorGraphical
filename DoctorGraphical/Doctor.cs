c20deee6aae826b564f5c9fe05ce52cb7abf9e80
Benjamin Goff <bgoff@mainerobotics.org>
2021-01-20 09:03:38 -0500

    Add project files.

---
+++ DoctorGraphical/Doctor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorGraphical
+{
+    class Doctor
+    {
+        static string[] hedges = {"Please tell me more.",
+          "Many of my patients tell me the same thing.",
+          "Please coninue." };
+
+        static string[] qualifiers = {"Why do you say that ",
+              "You seem to think that ",
+              "Can you explain why " };
+
+        static Dictionary<string, string> replacements = new Dictionary<string, string>()
+        {
+            {"i", "you" },
+            {"me", "you" },
+            {"we", "you" },
+            {"us", "you" },
+            {"mine", "yours"},
+            {"my", "your" },
+            {"am", "are" },
+        };
+
+        
+
+        /*
+         * Reply
+         * Returns a string based on the users input or 
+         * something to prompt them to write some more.
+         */
+        public static string Reply(string sentence)
+        {
+            Random rand = new Random();
+            int probability = rand.Next(1, 5);
+            int choice;
+
+            if (probability == 1)
+            {
+                choice = rand.Next(0, hedges.Length);
+                return hedges[choice];
+
+            }
+            else
+            {
+                choice = rand.Next(0, qualifiers.Length);
+                return qualifiers[choice] + ChangePerson(sentence);
+            }
+        }
+        /*
+         * ChangePerson
+         * Goes through th inputed sentence and changes pronouns from 
+         * first person to second person
+         * 
+         */
+        private static string ChangePerson(string sentence)
+        {
+            List<string> replyWords = new List<string>();
+            string[] words = sentence.Split(' '); //creating array of substrings split apart where the original string has spaces
+
+
+            foreach (string word in words)
+            {
+
+                /*
+                 * Checking to see if our key is in the dictionary and saving value to the reply if if it is
+                 * else we use the key as part of the reply 
+                 */
+                if (replacements.TryGetValue(word.ToLower(), out string wordValue))
+                {
+                    replyWords.Add(wordValue);
+                }
+                else
+                {
+                    replyWords.Add(word.ToLower());
+                }
+
+            }
+
+            return string.Join(" ", replyWords);
+        }
+    }
+}
---
+++ DoctorGraphical/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace DoctorGraphical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputRTFBox = new System.Windows.Forms.RichTextBox();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputRTFBox
+            // 
+            this.outputRTFBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputRTFBox.Location = new System.Drawing.Point(88, 12);
+            this.outputRTFBox.Name = "outputRTFBox";
+            this.outputRTFBox.Size = new System.Drawing.Size(594, 315);
+            this.outputRTFBox.TabIndex = 0;
+            this.outputRTFBox.Text = "Good morning, I hope you are well today.\nWhat can I do for you ?";
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextbox.Location = new System.Drawing.Point(97, 359);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(384, 29);
+            this.inputTextbox.TabIndex = 1;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(531, 359);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(82, 29);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.inputTextbox);
+            this.Controls.Add(this.outputRTFBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputRTFBox;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.Button enterButton;
+    }
+}
+
